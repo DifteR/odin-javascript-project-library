19b1a558fcc9dddc3f6200e11997a2c6a1020e88
Tilen Marš <its0black0magic@gmail.com>
2023-08-24 15:22:34 +0000

    Add "read" functionality

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function Book(title, author, year, pages, languange, edition) {
     this.pages = pages;
     this.language = languange;
     this.edition = edition;
+    this.read = false;
 }
 
 function addBookToLibrary(title, author, year, pages, language, edition) {
@@ -21,8 +22,9 @@ function submitForm(event) {
     var pages = document.getElementById("pages").value;
     var language = document.getElementById("languange").value;
     var edition = document.getElementById("edition").value;
+    var read = false;
 
-    addBookToLibrary(title, author, year, pages, language, edition);
+    addBookToLibrary(title, author, year, pages, language, edition, read);
     displayListOfBooks();
     console.table(myLibrary);
 }
@@ -30,7 +32,7 @@ function submitForm(event) {
 var form = document.getElementById("addBook");
 form.addEventListener("submit", submitForm);
 
-addBookToLibrary("example", "example", 1000, 250, "example", "example")
+addBookToLibrary("example", "example", 1000, 250, "example", "example", false)
 
 
 function displayListOfBooks(){
@@ -47,7 +49,9 @@ function displayListOfBooks(){
         const pagesCell = newRow.insertCell(3);
         const languangeCell = newRow.insertCell(4);
         const editionCell = newRow.insertCell(5);
-        const deleteButton = newRow.insertCell(6);
+        const readCell = newRow.insertCell(6);
+        const readButton = newRow.insertCell(7);
+        const deleteButton = newRow.insertCell(8);
 
         titleCell.textContent = book.title;
         authorCell.textContent = book.author;
@@ -55,6 +59,8 @@ function displayListOfBooks(){
         pagesCell.textContent = book.pages;
         languangeCell.textContent = book.language;
         editionCell.textContent = book.edition;
+        readCell.textContent = book.read;
+        readButton.innerHTML = `<input type='button' value='✓' id='read-button' data-id='${i}' onclick='markAsRead(event)'>`;
         deleteButton.innerHTML = `<input type='button' value='X' id='delete-button' data-id='${i}' onclick='deleteID(event)'>`;
         i++;
     }
@@ -62,9 +68,20 @@ function displayListOfBooks(){
 
 function deleteID(event) {
     idToDelete = event.target.getAttribute("data-id");
-    console.log(idToDelete);
     myLibrary.splice(idToDelete, 1);
     displayListOfBooks();
+}
+
+function markAsRead(event) {
+    id = event.target.getAttribute("data-id");
+    if (myLibrary[id].read == false) {
+        myLibrary[id].read = true;
+        displayListOfBooks();
+    }
+    else{
+        myLibrary[id].read = false;
+        displayListOfBooks();
+    }
 }
 
 var dataTable = document.getElementById("data-table");
